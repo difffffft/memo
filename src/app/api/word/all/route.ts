eb58214249a266b9d914a5d1e3ff06361fff2dce
DIFFFFFFT <672228275@qq.com>
2024-07-26 17:32:49 +0800

    reverse

--- src/app/api/word/all/route.ts
+++ src/app/api/word/all/route.ts
@@ -3,5 +3,6 @@ import { NextResponse, type NextRequest } from "next/server";
 
 export async function POST(request: NextRequest, response: NextResponse) {
   const words = await WordModel.find();
+  words.reverse()
   return new Response(JSON.stringify(words));
 }
